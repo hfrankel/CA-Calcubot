5bd56cfd1d836e069e740c9fa86c6bda4627b2e7
hfrankel <hfranks92@gmail.com>
2019-09-04 14:08:13 +1000

    Draft 1 opening credits added

---
+++ index.rb
@@ -0,0 +1,31 @@
+# Welcome message
+
+puts "Calcubot\n\n"
+
+# p "\e[2J\e[f" --> this is potentially a way to clear the screen.
+
+# Opening credits
+
+puts "2066\n\n"
+
+puts "The mad scientist Dr. Henrikh Finklefeinstein has launched his latest
+invention.\n
+Calcubot.\n
+Calcubot is a blank robot, designed to learn surrounding behaviour.\n
+One day in Dr. Finklefeinstein's lab, he is playing catch with Calcubot.
+Calcubot has recently spent time watching dog videos on YouTube and so when
+Dr. Finklefeinstein throws the ball at pace across the room, Calcubot chases it
+like a lunatic.\n
+Unfortunately for Calcubot and Dr. Finklefeinstein, the robot slams into the
+doctor's time & universe machine sitting in the corner and engages
+the time mechanism.\n
+Calcubot is sent back in time 1000 years to an alternate universe...\n"
+
+# Chapter 1
+
+gets.chomp
+
+puts "...and lands in Angland, Yourope.\n
+Calcubot is suddenly in danger. It's square in the middle of 4 menacing humans.\n
+They attack Calcubot.\n
+What is that thing?!"
