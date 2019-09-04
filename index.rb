4720491cf0bdf27da6e0159f85b853f84cea141b
hfrankel <hfranks92@gmail.com>
2019-09-04 14:41:28 +1000

    Added draft Chapters 1-3

--- index.rb
+++ index.rb
@@ -1,31 +1,69 @@
-# Welcome message
+# Welcome message // opening credits
 
-puts "Calcubot\n\n"
-
-# p "\e[2J\e[f" --> this is potentially a way to clear the screen.
-
-# Opening credits
-
-puts "2066\n\n"
-
-puts "The mad scientist Dr. Henrikh Finklefeinstein has launched his latest
-invention.\n
-Calcubot.\n
-Calcubot is a blank robot, designed to learn surrounding behaviour.\n
-One day in Dr. Finklefeinstein's lab, he is playing catch with Calcubot.
-Calcubot has recently spent time watching dog videos on YouTube and so when
-Dr. Finklefeinstein throws the ball at pace across the room, Calcubot chases it
-like a lunatic.\n
-Unfortunately for Calcubot and Dr. Finklefeinstein, the robot slams into the
-doctor's time & universe machine sitting in the corner and engages
-the time mechanism.\n
-Calcubot is sent back in time 1000 years to an alternate universe...\n"
-
-# Chapter 1
+puts "Calcubot"
 
 gets.chomp
 
-puts "...and lands in Angland, Yourope.\n
-Calcubot is suddenly in danger. It's square in the middle of 4 menacing humans.\n
-They attack Calcubot.\n
-What is that thing?!"
+puts "Chapter 1
+
+2066
+
+The mad scientist Dr. Henrikh Finklefeinstein has launched his latest
+invention.
+
+Calcubot.
+
+Calcubot is a blank robot, designed to learn surrounding behaviour.
+
+One day in Dr. Finklefeinstein's lab, he is playing catch with Calcubot.
+Calcubot has recently spent time watching dog videos on YouTube and so when
+Dr. Finklefeinstein throws the ball at pace across the room, Calcubot chases it
+like a lunatic.
+
+Unfortunately for Calcubot and Dr. Finklefeinstein, the robot slams into the
+doctor's time & universe machine sitting in the corner and engages
+the time mechanism.
+
+Calcubot is sent back in time 1000 years to an alternate universe..."
+
+gets.chomp
+
+puts "Chapter 2
+
+...and lands in Angland, Yourope.
+
+Calcubot is suddenly in danger. It's square in the middle of
+4 menacing humans.
+
+They attack Calcubot.
+
+What is that thing?!
+
+OPTIONS:
+
+(1) Surrender peacefully
+(2) Fight back and subdue
+
+Calcubot encounters Alouise Smyth, a witch posessing a magical staff."
+
+gets.chomp
+
+puts "Chapter 3
+
+Whittaker Forest, Angland
+
+Alouise takes Calcubot to her home. They make a plan on how to get Calcubot
+back to its world. Alouise agrees to accompany Calcubot and cast a spell to
+make it appear as a human. After struggling to make a solid plan, Alouise
+suggests that they see The Oracle, a dangerous and powerful being.
+
+On the way out, Calcubot notices a strange item on a broken shelf.
+
+OPTIONS:
+(1) repair shelf
+(2) leave
+(3) steal strange item"
+
+gets.chomp
+
+
