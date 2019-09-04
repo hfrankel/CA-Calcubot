1e9cfa140b6c9c5fe98046053dbc9a26aea32e18
hfrankel <hfranks92@gmail.com>
2019-09-04 15:10:00 +1000

    Added draft chapters 4-10

--- index.rb
+++ index.rb
@@ -66,4 +66,159 @@ OPTIONS:
 
 gets.chomp
 
+puts "Chapter 4
 
+In the woods on the way to the Oracle. An aggressive, wild bear approaches.
+Alouise says not to worry about the bear. It can't hurt them.
+
+OPTIONS:
+(1) leave the bear alone
+(2) throw a small rock to distract the bear
+(3) attack the bear"
+
+gets.chomp
+
+puts "Chapter 5
+
+Meeting with The Oracle. The Oracle says she will tell Calcubot how to get home.
+But for a price. She wants his gold antenna for her collection of rare items.
+
+\"How can you see my antenna?\"
+
+\"I'm The Oracle...\"
+
+OPTIONS:
+(1) passionately plea with the Oracle to give answer without giving up antenna
+(2) give up gold antenna
+(3) fight The Oracle
+
+The Oracle does not know exactly where Calcubot must go to return home but she
+has a feeling that the answer to that question can be found at
+Shady Pines in Bladderpool."
+
+gets.chomp
+
+puts "Chapter 6
+
+Our gang find themselves in Bladderpool. Alouise says she is going to go to
+the Shady Pines Tavern to try get more information. She tells Calcubot to
+stay where he is and not interact with anyone. Calcubot is looking around and
+sees an old man motion towards him. Calcubot approaches.
+
+The old man has a classic shell game in front of him and asks if Calcubot
+wants to play. The min fee is 1 item and the old man will double whatever the
+player gambles. An impossibly large younger man makes his way to
+the shell game table.
+
+\"Well Dad, looks like we got a player here. Sir, what's it gonna be?\"
+
+OPTIONS:
+(1) Politely decline
+(2) Gamble with money you don't have
+(3) Gamble with money you don't have
+
+Meet back up with Alouise who has more information. It is as Alouise feared.
+There is only one item in Yourope that might be able to help. That is
+Schvaytee's Pocketwatch. A powerful watch that can manipulate time. It is
+in the posession of the Daemon, Greg on the Isle of Dawn.
+
+We must find a ship that can take us. The closest port is at Portsmath.
+Calcubot and Alouise head to Portsmath."
+
+gets.chomp
+
+puts "Chapter 7
+
+Portsmath, Angland
+
+Calcubot and Alouise start their search at Shipmaster's Gate, where they come
+across the Shipmaster.
+
+\"I'm sorry but there are no vessles that go to the Isle of Dawn. You will have
+to find your own or somehow convince one of those mad bastards down by Shawshank
+Docks to take you ...and they take a lot of convincing.\"
+
+\"I'm sure we can work something out.\"
+
+\"Try Ol' Gammy leg. She'd be the only one to dare do it.\"
+
+At the docks... Calcubot spots what looks to be Ol Gammy Leg.
+
+OPTIONS:
+(1) Catch up to \"Ol' Gammy Leg\" and politely ask for her help
+(2) Shout \"Ol' Gammy Leg!\" then ask for her help
+(3) Shout \"Oi!\" and whistle at Gammy then demand her help"
+
+gets.chomp
+
+puts "Chapter 8
+
+Isle of Dawn, off Angland Coast
+
+Alouise and Calcubot wade through a thick jungle.
+
+They spot light in the distance. It's coming from a small tavern. They enter.
+
+A bedraggled man is sitting alone at the bar. The only company present. He
+reaches into his coat and pulls out a pointy metallic object with a wooden
+handle. Sticking it right into Calcubot's face, he pulls his thumb back and the
+device clicks.
+
+\"What is that?\" blurts Alouise.
+
+\"A weapon that hasn't been invented for 300 years,\" responds Calcubot.
+
+\"The machine knows.\" Greg puts away the device. \"You can have the watch.\"
+
+\"Why are you just giving it to us?\"
+
+\"Just take it!!!\" screams the daemon. \"It is cursed.\"
+
+Calcubot takes the watch from Greg.
+
+OPTION:
+(1) equip Schvaytee's Pocketwatch
+
+Calcubot and Alouise exit."
+
+gets.chomp
+
+puts "Chapter 9
+Isle of Dawn, off Angland Coast
+
+Calcubot and Alouise are heading back to Gammy's ship.
+
+\"The watch please?\"
+
+Calcubot turns and Alouise has her staff pointed at in its direction.
+
+\"Why?\"
+
+\"Let's just say I don't enjoy the threat of being burned at the stake.
+I figure I'll take my chances in some other time period.\"
+
+Calcubot is frozen.
+
+\"Now!\"
+
+OPTIONS:
+(1) try talk Alouise into letting you keep the pocketwatch
+(2) fight Alouise [EXT]"
+
+gets.chomp
+
+puts "Chapter 10a
+
+Calcubot returns home using Schvaytee's Pocketwatch, much to his creator,
+Dr. Finklefeinstein's joy.
+
+Calcubot asks if he can go time travelling again. It would like to study
+humanity in other time periods and universes."
+
+puts "Chapter 10b
+
+Calcubot returns home using Schvaytee's Pocketwatch, much to his creator,
+Dr. Finklefeinstein's joy.
+
+Calcubot discusses in depth his horrific experiences. Dr. Finklefeinstein
+decides to decommission Calcubot to study it closer."
